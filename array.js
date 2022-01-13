69a16c68a7073aaeba686e59a0e1d706744ba6b5
angelDariaux <angeldariaux@gmail.com>
2022-01-13 16:03:09 -0600

    terminamos de hacer el metodo de myDelete

--- array.js
+++ array.js
@@ -1,6 +1,6 @@
 // const array = ['diego', 'karen', 'Oscar',];
 
-class myArra {
+class MyArray {
   constructor() { 
     this.length = 0;
     this.data = {}
@@ -13,5 +13,27 @@ class myArra {
     this.length++;
     return this.data;
   }
+  pop () { 
+    const lastItem = this.data[this.length - 1]; // creamos esta constante solo para saber cual fue el ultimo item eliminado
+    delete this.data[this.length - 1]; // el keyword 'delete' borramos el elemento adentro del metodo
+    this.length--;
+    return lastItem;
+  }
+  myDelete (index) { 
+    const item = this.data[index]; // aqui solo nos regresa el item que eliminamos.
+    this.shiftIndex(index);
+
+    return item;
+  }
+  shiftIndex (index) { 
+    for(let i = index; i < this.length - 1; i++) { //Esta funcion empuja el index seleccionando para luego eliminar el ultimo indice con la funcion de abajo.
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
 }
+
+const myArray = new MyArray;    //creada nueva instancia
+
 
