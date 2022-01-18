6a4c08222d4e8c20d664dbd3b1659e33acdca03f
angelDariaux <angeldariaux@gmail.com>
2022-01-17 19:26:31 -0600

    terminado el curso baside estruturas de datos

--- tree/trees.js
+++ tree/trees.js
@@ -16,13 +16,15 @@ class BinarySearchTree {
     } else { 
       let currentNode = this.root;
       while (true) {
-        if (value < currentNode.value) {
+        if (value < currentNode.value) { //validacion por si el valor es menor(lado izquierdo)
           if (!currentNode.left) {
             currentNode.left = newNode;
 
             return this;
           }   
-        } else { 
+          currentNode = currentNode.left;
+
+        } else {            //(value > currentNode.value)validacion por si el valor es mayor (lado derecho)
           if (!currentNode.right) {
             currentNode.right = newNode;
 
@@ -36,3 +38,22 @@ class BinarySearchTree {
 }
 
 let myBinarySearchTree = new BinarySearchTree;
+
+myBinarySearchTree.insert(10);
+myBinarySearchTree.insert(4);
+myBinarySearchTree.insert(20);
+myBinarySearchTree.insert(2);
+myBinarySearchTree.insert(8);
+myBinarySearchTree.insert(17);
+myBinarySearchTree.insert(170);
+
+myBinarySearchTree.insert(1); // left del nodo del numero 2
+myBinarySearchTree.insert(45);// right del nodo del numero 170
+
+myBinarySearchTree.insert(9);
+
+
+
+
+console.log(myBinarySearchTree)
+
